973107df474eeb276ee215fb7721988df987cb11
Ben Best <ben@ecoquants.com>
2019-10-23 14:53:19 -0700

    clarify running API

--- run_api.R
+++ run_api.R
@@ -1,3 +1,6 @@
+# run API by sourcing this R script in RStudio
 library(plumber)
-r <- plumb("plumber.R")  # Where 'plumber.R' is the location of the file shown above
+r <- plumb("plumber.R")
 r$run(port=8000)
+# open in web browser: http://localhost:8000/__swagger__/
+# for more, see https://www.rplumber.io/docs
