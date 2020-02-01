03081d3c708b196cad6b6b2ccd4961a3465a3bec
Ben Best <ben@ecoquants.com>
2020-01-31 21:35:55 -0800

    full path to plumber.R on server

--- run_api.R
+++ run_api.R
@@ -1,6 +1,6 @@
 # run API by sourcing this R script in RStudio
 library(plumber)
-r <- plumb("plumber.R")
+r <- plumb("/srv/ws-api/plumber.R")
 #r$run(port=8888, host="0.0.0.0")
 r$run(port=8888)
 # open in web browser: http://localhost:8888/__swagger__/
