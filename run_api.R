b49fe422ce63f0f5185eb90c59d79ab68c5e8841
Ben Best <ben@ecoquants.com>
2020-01-31 21:17:09 -0800

    api update

--- run_api.R
+++ run_api.R
@@ -1,6 +1,7 @@
 # run API by sourcing this R script in RStudio
 library(plumber)
 r <- plumb("plumber.R")
-r$run(port=8000)
-# open in web browser: http://localhost:8000/__swagger__/
+#r$run(port=8888, host="0.0.0.0")
+r$run(port=8888)
+# open in web browser: http://localhost:8888/__swagger__/
 # for more, see https://www.rplumber.io/docs
