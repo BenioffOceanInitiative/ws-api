66467efae1573bca97e698624824d608fa9db5c0
Ben Best <ben@ecoquants.com>
2020-04-09 00:20:43 +0000

    old comment wrt Rscript

--- run_api.R
+++ run_api.R
@@ -17,7 +17,7 @@ plumber$new(plumber_r)$run(port=8888, host="0.0.0.0", swagger = T)
 # for more, see https://www.rplumber.io/docs
 
 # To start on rstudio.ships4whales.org:
-#   Rscript /srv/ws-api/run_api.R &
+#   sudo su root; Rscript /srv/ws-api/run_api.R &; exit
 # To restart the process:
 #   ps -eaf | grep ws-api
 #   # admin      494   442  0 Feb07 pts/0    00:00:08 /usr/local/lib/R/bin/exec/R --no-save --no-restore --slave --no-restore --file=/srv/ws-api/run_api.R
