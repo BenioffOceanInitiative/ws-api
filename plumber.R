a4c2ddc7f5a543b39c8474854bde26936ed6bdd3
Ben Best <ben@ecoquants.com>
2020-04-09 22:28:50 +0000

    working R chunk of BigQuery result into GeoJSON

--- plumber.R
+++ plumber.R
@@ -155,6 +155,9 @@ function(mmsi = NULL, date_beg = NULL, date_end = NULL, bbox = NULL){
   segs <- st_read(dsn = con, query = sql)
   
   
+  
+  
+  
   # geojson example
   nc = st_read(system.file("shape/nc.shp", package="sf"))
   write_sf(nc, "nc.geojson")
@@ -163,12 +166,82 @@ function(mmsi = NULL, date_beg = NULL, date_end = NULL, bbox = NULL){
   
   segs <- dbGetQuery(
     con, 
-    "SELECT mmsi, timestamp, lon, lat, ST_AsGeoJSON(linestring) as ln_geojson FROM gfw_ihs_segments LIMIT 3")
+    "SELECT mmsi, timestamp, lon, lat, ST_AsGeoJSON(linestring) as ln_geom FROM gfw_ihs_segments LIMIT 3")
+  
+  # convert dataframe with a GeoJSON column to R sf object to then output all as GeoJSON
+  # https://gis.stackexchange.com/questions/324863/manipulating-polygon-data-imported-from-google-bigquery-gis-within-r
+  segs$geom <- do.call(rbind, lapply(segs$ln_geom, read_sf))$geometry
+  st_geometry(segs) <- segs$geom
+  segs <- segs %>% select(-ln_geom)
+  
+  #show 
+  segs
+  # Simple feature collection with 3 features and 4 fields
+  # geometry type:  LINESTRING
+  # dimension:      XY
+  # bbox:           xmin: -119.8206 ymin: 33.7407 xmax: -118.1996 ymax: 34.16744
+  # epsg (SRID):    4326
+  # proj4string:    +proj=longlat +datum=WGS84 +no_defs
+  # # A tibble: 3 x 5
+  # mmsi timestamp             lon   lat                                     geom
+  # <int> <dttm>              <dbl> <dbl>                         <LINESTRING [°]>
+  #   1 477136800 2018-05-18 22:36:39 -118.  33.7   (-118.1996 33.7407, -118.1996 33.7408)
+  # 2 636016519 2018-07-31 22:19:28 -118.  33.8 (-118.2181 33.75612, -118.2181 33.75612)
+  # 3 218092000 2019-07-19 16:32:02 -120.  34.2 (-119.8206 34.16744, -119.8166 34.16654)
+  writeLines(sf_geojson(segs), "segs5.geojson")
+  # open geojson file and paste contents into http://geojson.io
+  
+  
+  segs_sf
   
   jsonlite::toJSON(segs)
   
   cat(toJSON(segs, auto_unbox = TRUE))
   writeLines(toJSON(segs, auto_unbox = TRUE), "segs.geojson")
+  
+  library(purrr)
+  library(tidyr)
+  
+  segs_x <- segs_df %>% 
+    nest(properties = -contains("ln_geojson")) %>% 
+    mutate(
+     geometry = map(ln_geojson, fromJSON)) %>% 
+    select(-ln_geojson)
+  segs_x %>% select(-geometry) %>% toJSON()
+  
+  writeLines(toJSON(segs_x, auto_unbox = TRUE, pretty = T), "segs4.geojson")
+  jsonlite::toJSON(segs_x)
+  
+  geojsonio::as.json(segs_x)
+  segs_x <- segs_df$ln_geojson
+  
+  segs_ls <- list(
+    "type" = "FeatureCollection",
+    "crs"  = list(
+      "type" = "name", 
+      "properties" = list(
+        "name"= "urn:ogc:def:crs:EPSG::4326" )),
+    "features"=c(
+      list(
+        "type" = "Feature",
+        "properties" = segs_df %>% slice(1) %>% select(-ln_geojson) %>% as.list(),
+        "geometry"   = segs_df %>% slice(1) %>% pull(ln_geojson) %>% fromJSON()),
+      list(
+        "type" = "Feature",
+        "properties" = segs_df %>% slice(1) %>% select(-ln_geojson) %>% as.list(),
+        "geometry"   = segs_df %>% slice(1) %>% pull(ln_geojson) %>% fromJSON())))
+    
+  writeLines(toJSON(segs_ls, auto_unbox = T, pretty = T), "segs2.json")
+  
+    [
+      { "type": "Feature", 
+        "properties": {"mmsi":351819000,"timestamp":"2019-12-18 23:28:05","lon":-118.2383,"lat":33.7517},
+        "geometry": 
+    
+    )
+  
+  toJSON(segs_ls, auto_unbox = T, pretty = T)
+  jsonify::as.json(segs_ls)
   
   segs <- as.list(segs)
   segs$geometry = unbox(segs$ln_geojson)
