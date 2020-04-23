8c84cf3635ea63565ea911b32aaf32e29e67cb8f
SeanGoral <seangoral@ucsb.edu>
2020-04-23 21:16:30 +0000

    @bbest I added a lil bit to the api. It's spitting out jsons and geojsons but they're not exactly valid... I'll keep working

---
+++ api.py
@@ -0,0 +1,46 @@
+import flask
+from flask import request, jsonify
+import json
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+#FILL IN YOUR PATH TO THE 'Benioff Ocean Initiative-454f666d1896.json'
+credentials_json = '/Users/seangoral/bq_api_test/venv/Benioff Ocean Initiative-454f666d1896.json'
+
+credentials = service_account.Credentials.from_service_account_file(credentials_json)
+
+project_id = 'benioff-ocean-initiative'
+client = bigquery.Client(credentials= credentials,project=project_id)
+
+# http://127.0.0.1:5000/api
+@app.route('/api', methods=['GET'])
+def home():
+    return '''<h1>WhaleSafe API</h1>
+<p>A prototype API for AIS Message json and geojson.</p>'''
+
+# http://127.0.0.1:5000/api/v1/geo
+@app.route('/api/v1/geo', methods=['GET'])
+def api_geo():
+    query_job = client.query("""SELECT 
+                             ST_ASGEOJSON(linestring) as geojson 
+                             FROM `benioff-ocean-initiative.clustered_datasets.gfw_ihs_segments` 
+                             WHERE DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY);""")  
+    records = [dict(row) for row in query_job]
+    json_obj = json.dumps(str(records))
+    result = json_obj.replace("\\", "") 
+    return (result)
+
+@app.route('/api/v1/resources/stats', methods=['GET'])
+def api_stats():
+    query = """SELECT * FROM `benioff-ocean-initiative.whalesafe_ais.mmsi_cooperation_stats`;"""
+    query_job = client.query(query)
+    records = [dict(row) for row in query_job]
+    json_obj = json.dumps(str(records))
+    result = json_obj.replace("\\", "") 
+
+    return(result)                       
+
+app.run()
