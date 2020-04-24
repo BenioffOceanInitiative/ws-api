21a7f1a54636d5e852d041b7d00ea0b22684b2fb
SeanGoral <seangoral@ucsb.edu>
2020-04-24 19:59:00 +0000

    @bbest and @mvisalli got a reall basic api sorta framed out. I had to use pandas to go from BigQuery to pandas to geojson. but when run, http://127.0.0.1:5000/api/v1/geo_2 spits out a valid geojson. http://127.0.0.1:5000/api/v1/stats/mmsi spits out a basic ship score json.     http://127.0.0.1:5000/api/v1/stats/operator spits out a basic operator score json. now some add some params to make the BQ sql editable.

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 import flask
 #from flask import request, jsonify
-from pandas import DataFrame
+import pandas as pd
 import json
+import geojson
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
@@ -23,8 +24,8 @@ def home():
 <p>A prototype API for AIS Message json and geojson.</p>'''
 
 # http://127.0.0.1:5000/api/v1/geo_1
-@app.route('/api/v1/geo_1', methods=['GET'])
-def api_geo_1():
+@app.route('/api/v1/geo', methods=['GET'])
+def api_geo():
     
     sql = ("""SELECT 
            ST_ASGEOJSON(linestring) as linestring
@@ -37,27 +38,28 @@ def api_geo_1():
 
     return(result)  
     
-# http://127.0.0.1:5000/api/v1/geo
-@app.route('/api/v1/geo', methods=['GET'])
-def api_geo():
+# http://127.0.0.1:5000/api/v1/geo_1
+@app.route('/api/v1/geo_1', methods=['GET'])
+def api_geo_1():
     
     sql = ("""SELECT 
            CAST(mmsi AS STRING) AS mmsi, 
            CAST(timestamp AS STRING) AS timestamp, 
-           ST_ASGEOJSON(linestring) as linestring
+           CAST(lon AS STRING) AS lon,
+           CAST(lat AS STRING) AS lat
+           
            FROM `benioff-ocean-initiative.clustered_datasets.gfw_ihs_segments` 
            WHERE DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY);""") 
     df = client.query(sql).to_dataframe()
-    df['linestring'] = df['linestring'].replace(["\\"], "")
     
-    def df_to_geojson(df, properties, line = "linestring"):
+    def df_to_geojson(df, properties, lat='lat', lon='lon'):
         geojson = {'type':'FeatureCollection', 'features':[]}
         for _, row in df.iterrows():
             feature = {'type':'Feature',
                        'properties':{},
-                       'geometry':{'type':'Linestring',
+                       'geometry':{'type':'Point',
                                    'coordinates':[]}}
-            feature['geometry']['coordinates'] = [row[line]]
+            feature['geometry']['coordinates'] = [row[lon],row[lat]]
             for prop in properties:
                 feature['properties'][prop] = row[prop]
             geojson['features'].append(feature)
@@ -66,6 +68,46 @@ def api_geo():
     cols = ['mmsi', 'timestamp']
     geojson = df_to_geojson(df, cols)
     return(geojson)
+
+# http://127.0.0.1:5000/api/v1/geo_2
+@app.route('/api/v1/geo_2', methods=['GET'])
+def api_geo_2():
+    # Query BigQuery table
+    sql = """SELECT 
+        lon, lat, lead_lon, lead_lat, 
+        CAST(mmsi AS FLOAT64) AS mmsi,
+        operator AS operator,
+        CAST(speed_knots AS FLOAT64) AS speed_knots, 
+        CAST(implied_speed_knots AS FLOAT64) AS implied_speed_knots,
+        CAST(calculated_knots AS FLOAT64) AS calculated_knots,
+        FROM `benioff-ocean-initiative.scratch.gfw_ihs_segments_test` 
+        WHERE DATE(timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY);"""
+
+    # Make into pandas dataframe
+    df = client.query(sql).to_dataframe()
+    
+    # data to geojson function...
+    def data2geojson(df):
+        features = []
+        insert_features = lambda X: features.append(
+                geojson.Feature(geometry=geojson.LineString(([X["lead_lon"], X["lead_lat"]],
+                                                             [X["lon"], X["lat"]])),
+                                properties=dict(mmsi=X["mmsi"],
+                                                operator=X["operator"],
+                                                speed_knots=X["speed_knots"],
+                                                implied_speed_knots=X["implied_speed_knots"],
+                                                calculated_knots=X["calculated_knots"])))
+        df.apply(insert_features, axis=1)
+        #with open('/Users/seangoral/bq_api_test/map1.geojson', 'w', encoding='utf8') as fp:
+        geojson_obj = geojson.dumps(geojson.FeatureCollection(features, indent=2, sort_keys=True), sort_keys=True, ensure_ascii=False)
+        return(geojson_obj)
+    # provide columns maybe...   
+    #col = ['lead_lat','lead_lon', 'lat', 'lon','mmsi','operator','speed_knots','implied_speed_knots','calculated_knots']
+    # combine df and columns
+    #data = pd.DataFrame(df, columns=col)
+
+    results = data2geojson(df)
+    return(results)
     
 @app.route('/api/v1/stats', methods=['GET'])
 def api_stats():
